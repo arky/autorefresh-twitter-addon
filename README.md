fedd8be9032bc90fed2d4e9576dd31edc07d08db
Arky <hitmanarky@gmail.com>
2012-10-17 00:05:20 +0700

    New addon, waiting for AMO review

---
+++ README.md
@@ -0,0 +1,4 @@
+Addon automatically refreshes twitter timeline to show new tweets on twitter.com
+
+Try it here:
+https://addons.mozilla.org/en-US/firefox/addon/twitter_autorefresh/
---
+++ data/refresh.js
@@ -0,0 +1,7 @@
+document.documentElement.addEventListener('DOMNodeInserted', function(e){
+  if (document.getElementsByClassName("new-tweets-bar").length) {
+       document.getElementsByClassName('new-tweets-bar')[0].click();
+  }
+}, false);
+
+
