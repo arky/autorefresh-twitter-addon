193c2361d89297f920184602980785c2ce3b0916
Arky <hitmanarky@gmail.com>
2014-07-26 13:49:01 +0300

    Code cleanup

--- data/refresh.js
+++ data/refresh.js
@@ -1,7 +1,8 @@
 document.documentElement.addEventListener('DOMNodeInserted', function(e){
-  if (document.getElementsByClassName("new-tweets-bar").length) {
+    if (document.getElementsByClassName("new-tweets-bar").length) {
        document.getElementsByClassName('new-tweets-bar')[0].click();
-  }
+	console.log("Reloading fresh Tweets");
+     }
 }, false);
 
 
