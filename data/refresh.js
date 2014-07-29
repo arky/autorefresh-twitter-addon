427f5856c109b46d5c748d5c5d531c302037583f
Arky <hitmanarky@gmail.com>
2014-07-29 11:00:12 +0300

    Storing Scroll Position of the page. Not good solution

--- data/refresh.js
+++ data/refresh.js
@@ -1,7 +1,12 @@
 document.documentElement.addEventListener('DOMNodeInserted', function(e){
     if (document.getElementsByClassName("new-tweets-bar").length) {
+	var Ypos = window.scrollY;
+	var Xpos = 0;
+	console.log(Ypos);
        document.getElementsByClassName('new-tweets-bar')[0].click();
 	console.log("Reloading fresh Tweets");
+	window.scrollTo(Xpos,Ypos);
+	console.log("Scrolling back to", Xpos, Ypos);
      }
 }, false);
 
