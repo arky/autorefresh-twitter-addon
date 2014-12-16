1c22d338d4d1525bd0e050664db8cef734fe11bd
Arky <hitmanarky@gmail.com>
2014-12-16 16:04:29 +0700

    Updated the latest experimental Addon-SDK

--- README.md
+++
@@ -1,4 +0,0 @@
-Addon automatically refreshes twitter timeline to show new tweets on twitter.com
-
-Try it here:
-https://addons.mozilla.org/en-US/firefox/addon/twitter_autorefresh/
--- data/refresh.js
+++ data/refresh.js
@@ -1,12 +1,12 @@
+//TODO 
+// Check if new-tweet-bar is present 
+// If so, click it.
+
+// Event listener
 document.documentElement.addEventListener('DOMNodeInserted', function(e){
     if (document.getElementsByClassName("new-tweets-bar").length) {
-	var Ypos = window.scrollY;
-	var Xpos = 0;
-	console.log(Ypos);
        document.getElementsByClassName('new-tweets-bar')[0].click();
 	console.log("Reloading fresh Tweets");
-	window.scrollTo(Xpos,Ypos);
-	console.log("Scrolling back to", Xpos, Ypos);
      }
 }, false);
 
