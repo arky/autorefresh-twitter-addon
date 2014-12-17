3bb54bfc1b98d372ebc966597b2c972a62db226b
Arky <hitmanarky@gmail.com>
2014-12-17 17:37:46 +0700

    Porting code to MutationObservers

--- data/refresh.js
+++ data/refresh.js
@@ -1,3 +1,5 @@
+/* Old Event Listern Code
+
 //TODO 
 // Check if new-tweet-bar is present 
 // If so, click it.
@@ -10,4 +12,25 @@ document.documentElement.addEventListener('DOMNodeInserted', function(e){
      }
 }, false);
 
+
+*/ 
+
+
+// select the target node
+var target = document.getElementsByClassName("new-tweets-bar");
+
+// create an observer instance
+var observer = new MutationObserver(function(mutations) {
+      if (document.getElementsByClassName("new-tweets-bar").length) {                                                                                           document.getElementsByClassName('new-tweets-bar')[0].click();                                                                                          console.log("Reloading fresh Tweets");      
+       };           
+});
+ 
+// configuration of the observer:
+var config = { attributes: true, childList: true, characterData: true };
+ 
+// pass in the target node, as well as the observer options
+observer.observe(target, config);
+ 
+// later, you can stop observing
+//observer.disconnect();
 
