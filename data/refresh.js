300145df1552c9b0f3c70d8665a7048057d0544b
Jeffrey Richman <jcrichman@rochester.rr.com>
2016-10-11 19:20:28 -0400

    Added a timeout call to delay refreshes
    Currently at 2 seconds

--- data/refresh.js
+++ data/refresh.js
@@ -1,6 +1,8 @@
+var timer_active = false;
 document.documentElement.addEventListener('DOMNodeInserted', function(e){
-    if (document.getElementsByClassName("new-tweets-bar").length) {
-	document.getElementsByClassName('new-tweets-bar')[0].click();
+    if (!timer_active && document.getElementsByClassName("new-tweets-bar").length) {
+	timer_active = true;
+	setTimeout(function() {timer_active = false; document.getElementsByClassName('new-tweets-bar')[0].click();}, 2000);
     }
 }, false);
 
