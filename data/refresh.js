0ee4eb2cda6115c8375fa91f4351e7d1277d3395
Jeffrey Richman <jcrichman@rochester.rr.com>
2016-10-11 19:32:51 -0400

    Update refresh.js

--- data/refresh.js
+++ data/refresh.js
@@ -1,8 +1,8 @@
-var timer_active = false;
+var timer_started = false;
 document.documentElement.addEventListener('DOMNodeInserted', function(e){
-    if (!timer_active && document.getElementsByClassName("new-tweets-bar").length) {
-	timer_active = true;
-	setTimeout(function() {timer_active = false; document.getElementsByClassName('new-tweets-bar')[0].click();}, 2000);
+    if (active && !timer_started && document.getElementsByClassName("new-tweets-bar").length) {
+	timer_started = true;
+	setTimeout(function() {timer_started = false; document.getElementsByClassName('new-tweets-bar')[0].click();}, 2000);
     }
 }, false);
 
