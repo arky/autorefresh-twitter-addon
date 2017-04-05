729c184ab88207b3b137e47ea1305907c144a3c5
Arky <hitmanarky@gmail.com>
2017-04-05 18:49:50 +0700

    Started porting to Webextentions API Refer https://github.com/arky/autorefresh-twitter-addon/issues/2

--- data/refresh.js
+++
@@ -1,6 +0,0 @@
-document.documentElement.addEventListener('DOMNodeInserted', function(e){
-    if (document.getElementsByClassName("new-tweets-bar").length) {
-	document.getElementsByClassName('new-tweets-bar')[0].click();
-    }
-}, false);
-
