f1a6756ee3c92d4cfa5dc8edeb643be77068667c
Arky <hitmanarky@gmail.com>
2017-04-08 12:50:21 +0700

    Debugging Revert
    Reverting to old version to test the basic functionality of the
    webextention

--- refresh.js
+++ refresh.js
@@ -1,8 +1,6 @@
-var timer_started = false;
+//TODO: Update this code with delay timer code.
 document.documentElement.addEventListener('DOMNodeInserted', function(e){
-    if (active && !timer_started && document.getElementsByClassName("new-tweets-bar").length) {
-	timer_started = true;
-	setTimeout(function() {timer_started = false; document.getElementsByClassName('new-tweets-bar')[0].click();}, 2000);
+    if (document.getElementsByClassName("new-tweets-bar").length) {
+	document.getElementsByClassName('new-tweets-bar')[0].click();
     }
 }, false);
-
